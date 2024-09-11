8d2b1d28cd9b5fbfc052f2d03bd02b9ee6e02c9e
Felipe Oliveira <felipeosouz4@gmail.com>
2024-09-11 18:55:53 -0300

    adicionando link ao dataset

--- mercadolivre_project/mercadolivre_project/spiders/mercadolivre.py
+++ mercadolivre_project/mercadolivre_project/spiders/mercadolivre.py
@@ -19,7 +19,7 @@ class MercadolivreSpider(scrapy.Spider):
             carro_year = carro_info[0] if len(carro_info) > 0 else 'N/A'
             carro_km = carro_info[1] if len(carro_info) > 1 else 'N/A'
 
-
+            
             yield {
                 "nome": carro.css("h2.ui-search-item__title::text").get(),
                 "preco": carro.css("span.andes-money-amount__fraction::text").get(),
@@ -27,7 +27,8 @@ class MercadolivreSpider(scrapy.Spider):
                 "km": carro_km,
                 "historico/vistoria": carro.css("span.ui-pb-label::text").get(),
                 "vendedor": carro.css("p.ui-search-official-store-label.ui-search-item__group__element.ui-search-color--GRAY::text").get(),
-                "localização": carro.css("span.ui-search-item__group__element.ui-search-item__location::text").get()
+                "localização": carro.css("span.ui-search-item__group__element.ui-search-item__location::text").get(),
+                "link": carro.css("div.ui-search-item__group.ui-search-item__group--title a::attr(href)").get()
             }
 
         if self.page_count < self.max_pages:
